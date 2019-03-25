0e0a07188bf33f3bb57d1d7b205ec32ba8169606
Imayhaveborkedit <imayhaveborkedit@gmail.com>
2019-03-25 03:38:26 -0400

    Remove useless SpeakingState enum and add Permissions-like variant
    The SpeakingState enum I added in my vwsV4 pr was only used in one place as a default argument value.  This is technically a breaking change but oh well I hope no one actually used that thing.

--- discord/__init__.py
+++ discord/__init__.py
@@ -50,6 +50,7 @@ from .webhook import *
 from .voice_client import VoiceClient
 from .audit_logs import AuditLogChanges, AuditLogEntry, AuditLogDiff
 from .raw_models import *
+from .speakingstate import SpeakingState
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
 
